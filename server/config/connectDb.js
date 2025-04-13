02f0b6b7f4076a345f84dcf265bbfaa3d5d60329
11milana11 <milanamilinskaia@gmail.com>
2025-04-13 12:09:41 +0500

    feat: add connection mongoDB

---
+++ server/config/connectDb.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+if(!process.env.MONGODB_URI) {
+    throw new Error (
+        "Please provide MONGODB_URI"
+    )
+}
+
+const connectDB = async () => {
+    console.log(process.env.MONGODB_URI);
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+    } catch(error) {
+        console.log("MongoDB connection Failed!!!", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
